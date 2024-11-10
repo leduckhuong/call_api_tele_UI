3d2c49303ba311cc3abcf26d644c2fe9a8b7e675
leduckhuong <leduckhuong@kali.kali.org>
2024-11-11 00:29:06 +0700

    use reactjs for search function to optimize performance

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const TeserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+    mode: 'production'
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [new TeserPlugin()],
+    // }
+}
